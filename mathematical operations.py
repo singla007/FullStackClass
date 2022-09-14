d4f1bb7ba6555cd574476167de2a55f1c917ed66
Yogesh Singla <eryogeshkumarsingla007@gmail.com>
2022-09-13 20:12:25 -0400

    Update mathematical operations.py

--- mathematical operations.py
+++ mathematical operations.py
@@ -1,13 +1,19 @@
-try:
-    input1 = int(input("Please input first number: "))
-    input2 = int(input("Please input second number: "))
-    
-    print("Subtraction = ",input1-input2)
-    print("Addition = ",input1+input2)
-    print("Division = ",input1/input2)
-    print("Multiplication = ",input1*input2)
-    print("Power = ",input1**input2)
-except:
-    print("Please input correct datatype for mathematical functions")
-
-
+while True:
+    try:
+        input1 = int(input("Please input first number: "))
+        input2 = int(input("Please input second number: "))
+        
+        if input2==0:
+            print("Division is not possible with 0 in denominator, try again with some other value")
+            continue
+        
+        print("Subtraction = ",input1-input2)
+        print("Addition = ",input1+input2)
+        print("Division = ",input1/input2)
+        print("Multiplication = ",input1*input2)
+        print("Power = ",input1**input2)
+        
+        if int(input("Press 0 to quit and any other number to continue: "))==0:
+            break
+    except:
+        print("Please input correct datatype for mathematical functions")
