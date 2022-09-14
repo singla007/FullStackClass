b42446d27dc2a6cd4e1325e0503abfcdd930138e
Yogesh Singla <eryogeshkumarsingla007@gmail.com>
2022-09-13 20:06:27 -0400

    Add files via upload

---
+++ mathematical operations.py
@@ -0,0 +1,13 @@
+try:
+    input1 = int(input("Please input first number: "))
+    input2 = int(input("Please input second number: "))
+    
+    print("Subtraction = ",input1-input2)
+    print("Addition = ",input1+input2)
+    print("Division = ",input1/input2)
+    print("Multiplication = ",input1*input2)
+    print("Power = ",input1**input2)
+except:
+    print("Please input correct datatype for mathematical functions")
+
+
